8d7cf9f4d3d46698c0b5ffcac11b1f6b4d7cbb40
Alex Liao <alex197445@gmail.com>
2024-09-05 21:02:25 -0700

    Add sliding window

--- src/xpr_feature_reshaping.py
+++ src/xpr_feature_reshaping.py
@@ -38,17 +38,24 @@ import numpy as np
     37: Packet size (B) [Average]
 
     This list comes from src/CSharp_Tool_BGP/Readme.md
+    The last column is the label. It is a binary value, 1 for anomaly and -1 for normal.
+
+    @param arr: numpy array, the dataset to be reshaped
+    @param n: int, the number of rows to be aggregated
+    @param sliding_window: bool, whether to use a sliding window. 
+    If not using a sliding window, the rows of the output will be 1/n of the input.
+    If use a sliding window, the rows of the output will be the same as the input, 
+    and each row will be the aggregation of the previous n rows.
+    For example, if input is [[1], [1], [1], [10], [10], [10], [1], [1], [1], [1]] and n is 2, and we are summing the column, 
+    then the output will be: [[2], [11], [20], [2], [2]] without sliding_window.
+    With sliding window, the output will be: [[1], [2], [2], [11], [20], [20], [11], [2], [2], [2]]
+    Respectively, if the label column of the input is [[-1], [-1], [-1], [1], [1], [1], [-1], [-1], [-1], [-1]] and n is 2,
+    the output will be: [[-1], [1], [1], [-1], [-1]] without sliding_window.
+    With sliding window, the output will be: [[-1], [-1], [-1], [1], [1], [1], [1], [-1], [-1], [-1]]
 """
-def aggregate_rows(arr, n):
-    # Calculate the number of rows to keep to ensure the number of rows is a multiple of n
-    rows_to_keep = (arr.shape[0] // n) * n
-    arr = arr[:rows_to_keep]
-    
-    # Reshape the array to the shape (m/n, n, number of columns)
-    reshaped = arr.reshape(-1, n, arr.shape[1])
-    
+def aggregate_rows(arr, n, sliding_window=False):
     # Apply different aggregation functions to different columns
-    coloumns_to_keep_the_first = [0, 1, 2, 3]
+    coloumns_to_keep = [0, 1, 2, 3]
     # Set items with feature number.
     columns_to_sum = [1, 2, 3, 4, 8, 9, 10, 12, 34, 35, 36]
     columns_to_mean = [5, 7, 13, 37]
@@ -57,18 +64,109 @@ def aggregate_rows(arr, n):
     columns_to_sum = [x+3 for x in columns_to_sum]
     columns_to_mean = [x+3 for x in columns_to_mean]
     columns_to_max = [x+3 for x in columns_to_max]
+
+    if sliding_window:
+        return aggregate_rows_with_sliding_window(arr, n, coloumns_to_keep, columns_to_sum, columns_to_mean, columns_to_max)
+    else:
+        return aggregate_rows_directly(arr, n, coloumns_to_keep, columns_to_sum, columns_to_mean, columns_to_max)
+
+def aggregate_rows_directly(arr, n, coloumns_to_keep, columns_to_sum, columns_to_mean, columns_to_max):
+    # Calculate the number of rows to keep to ensure the number of rows is a multiple of n
+    rows_to_keep = (arr.shape[0] // n) * n
+    arr = arr[:rows_to_keep]
+    
+    # Reshape the array to the shape (m/n, n, number of columns)
+    reshaped = arr.reshape(-1, n, arr.shape[1])
     
     aggregated = np.zeros((reshaped.shape[0], reshaped.shape[2]))
-    aggregated[:, coloumns_to_keep_the_first] = reshaped[:, 0, coloumns_to_keep_the_first]  # Apply values of the first row in the group
+    aggregated[:, coloumns_to_keep] = reshaped[:, 0, coloumns_to_keep]  # Apply values of the first row in the group
     aggregated[:, columns_to_sum] = reshaped[:, :, columns_to_sum].sum(axis=1)
     aggregated[:, columns_to_mean] = reshaped[:, :, columns_to_mean].mean(axis=1)
     aggregated[:, columns_to_max] = reshaped[:, :, columns_to_max].max(axis=1)
-    aggregated[:, 41] = np.round(np.sum(reshaped[:, :, 41] == 1, axis=1)/n)*2-1  # Apply mean and round to the label
+    aggregated[:, -1] = np.round(np.sum(reshaped[:, :, -1] == 1, axis=1)/n)*2-1  # Apply mean and round to the label
     
     return aggregated
 
-def aggregate_datasets(datasets, time_span):
+def aggregate_rows_with_sliding_window(arr, n, coloumns_to_keep, columns_to_sum, columns_to_mean, columns_to_max):
+    
+    aggregated = np.zeros(arr.shape)    
+    for i in range(arr.shape[0]):
+        j = i+1
+        sliding_window = arr[j-(min(j,n)):j]
+        aggregated[i, coloumns_to_keep] = arr[i, coloumns_to_keep] # Just keep the value.
+        aggregated[i, columns_to_sum] = sliding_window[:, columns_to_sum].sum(axis=0)
+        aggregated[i, columns_to_mean] = sliding_window[:, columns_to_mean].mean(axis=0)
+        aggregated[i, columns_to_max] = sliding_window[:, columns_to_max].max(axis=0)
+        aggregated[i, -1] = np.round(np.sum(sliding_window[:, -1] == 1)/n)*2-1
+    
+    return aggregated
+
+def aggregate_datasets(datasets, time_span, sliding_window=False):
     aggregated_datasets = []
     for dataset in datasets:
-        aggregated_datasets.append(aggregate_rows(dataset, time_span))
+        aggregated_datasets.append(aggregate_rows(dataset, time_span, sliding_window))
     return aggregated_datasets
+
+# Test the function
+if __name__ == '__main__':
+    # Test the function
+    print("============================================")
+    arr = np.array([[1,1,1,1,-1], 
+                    [1,1,1,1,-1], 
+                    [1,1,1,1,-1], 
+                    [10,10,10,10,1], 
+                    [10,10,10,10,1], 
+                    [10,10,10,10,1], 
+                    [1,1,1,1,-1], 
+                    [1,1,1,1,-1], 
+                    [1,1,1,1,-1], 
+                    [1,1,1,1,-1]])
+    n = 2
+    columns_to_keep = [0]
+    columns_to_sum = [1]
+    columns_to_mean = [2]
+    columns_to_max = [3]
+    print("Test the function aggregate_rows without sliding window")
+    print("The input array is:")
+    print(arr)
+    output = aggregate_rows_directly(arr, n, columns_to_keep, columns_to_sum, columns_to_mean, columns_to_max)
+    print("columns_to_keep")
+    print("Expected output: 1, 1, 10, 1, 1")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, columns_to_keep].flatten()))))
+    print("columns_to_sum")
+    print("Expected output: 2, 11, 20, 2, 2")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, columns_to_sum].flatten()))))
+    print("columns_to_mean")
+    print("Expected output: 1, 5, 10, 1, 1")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, columns_to_mean].flatten()))))
+    print("columns_to_max")
+    print("Expected output: 1, 10, 10, 1, 1")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, columns_to_max].flatten()))))
+    print("Label")
+    print("Expected output: -1, -1, 1, -1, -1")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, -1].flatten()))))
+    print("--------------------------------------------")
+    print("Test the function aggregate_rows with sliding window")
+    output = aggregate_rows_with_sliding_window(arr, n, columns_to_keep, columns_to_sum, columns_to_mean, columns_to_max)
+    print("columns_to_keep")
+    print("Expected output: 1, 1, 1, 10, 10, 10, 1, 1, 1, 1")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, columns_to_keep].flatten()))))
+    print("columns_to_sum")
+    print("Expected output: 1, 2, 2, 11, 20, 20, 11, 2, 2, 2")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, columns_to_sum].flatten()))))
+    print("columns_to_mean")
+    print("Expected output: 1, 1, 1, 5, 10, 10, 5, 1, 1, 1")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, columns_to_mean].flatten()))))
+    print("columns_to_max")
+    print("Expected output: 1, 1, 1, 10, 10, 10, 10, 1, 1, 1")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, columns_to_max].flatten()))))
+    print("Label")
+    print("Expected output: -1, -1, -1, -1, 1, 1, -1, -1, -1, -1")
+    print("Actual   output:", ", ".join(map(str, map(int, output[:, -1].flatten()))))
+    print("--------------------------------------------")
+    print("Test sliding window with n=1")
+    n = 1
+    output = aggregate_rows_with_sliding_window(arr, n, columns_to_keep, columns_to_sum, columns_to_mean, columns_to_max)
+    print("Expected output:", arr)
+    print("Actual   output:", output)
+
